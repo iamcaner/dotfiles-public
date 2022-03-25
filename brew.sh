d5d09ece7b29b3256c5959383f40e2d5aca27f99
Caner <caner@canerk.com>
2022-03-25 13:28:33 +0100

    Remove duplicate scrcpy

--- brew.sh
+++ brew.sh
@@ -2,4 +2,4 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install thefuck exiftool openssl scrcpy python git tree scrcpy zsh-autosuggestions spoof-mac
+brew install thefuck exiftool openssl python git tree scrcpy zsh-autosuggestions spoof-mac
